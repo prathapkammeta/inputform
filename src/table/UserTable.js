680d9ccd83f262628bb21044a41063266aec0e0b
prathapkammeta <prathapkammeta@gmail.com>
2022-07-02 08:20:50 +0530

    2 files modified, 1 added, 1 deleted

---
+++ src/table/UserTable.js
@@ -0,0 +1,24 @@
+import React from 'react'
+const UserTable= props=>( 
+  
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>UserName</th>
+
+        <th>Actions</th>
+
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td>Name</td>
+        <td>UserName</td>
+
+        <td>Actions</td>
+
+        </tr>
+          </tbody>
+</table>
+)
